a0e86d310ce45b64c32a3d1522b26633f2f0de76
nimeshlathiya <142136394+NimeshLathiya@users.noreply.github.com>
2024-06-02 19:39:18 +0530

    Update Box.js

--- src/Components/ArrayComponents/Box.js
+++ src/Components/ArrayComponents/Box.js
@@ -2,7 +2,7 @@ import heart from "../../Components/Home/image/Group.png";
 import man from "../../Components/Home/image/healthicons_i-training-class.png";
 import empty from "../../Components/Home/image/mdi_trash-can-empty.png";
 
-let box = [
+let  = [
   {
     image: heart,
     text: "Information & Education",
